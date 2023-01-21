06fd835b76434a2e06569e21a96d60419b430e2e
agustina_heredia <cuadernomenstrual@gmail.com>
2023-01-21 13:33:48 +0100

    first commit

--- src/ListaDirectorio/ListarDirectorio.java
+++ src/ListaDirectorio/ListarDirectorio.java
@@ -1,22 +1,43 @@
 package ListaDirectorio;
-
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 
 public class ListarDirectorio {
 
-	public static void main(String[] args) {
-		
-		File directorio = new File("/Users/agustinaheredia/Documents");
-		
-		String[] nombre_archivos = directorio.list();
-		
-		Arrays.sort(nombre_archivos);
-		
-		for(int i = 0; i< nombre_archivos.length; i++) {
-			System.out.println(nombre_archivos[i]);
-		}
-		
-	}
+    public static void main(String[] args) {
 
+        File directorio = new File("/Users/agustinaheredia/Documents");
+        listarDirectorio(directorio);
+    }
+
+    public static void listarDirectorio(File directorio) {
+
+        
+        File[] contenido = directorio.listFiles();
+
+       
+        Arrays.sort(contenido);
+        
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("archivo.txt"))) {
+        	for (File archivo : contenido) {
+        	
+        		if (archivo.isDirectory()) {
+                    bw.write("Directorio - " + archivo.getName() + " - Fecha de modificación: " + new Date(archivo.lastModified()));
+                    bw.newLine(); // Inserta un salto de línea
+                    listarDirectorio(archivo);
+            	
+                    } else {
+                    	bw.write("Archivo - " + archivo.getName() + " - Fecha de modificación: " + new Date(archivo.lastModified()));
+                    	bw.newLine(); // Inserta un salto de línea
+                    }
+        	}
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
+        
+    }
 }
